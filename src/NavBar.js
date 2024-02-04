e43f52553de64b2e3dac75ad0333ad02c2ef6721
noga_gazit <noga.zahav@gmail.com>
2024-02-04 22:20:50 +0200

    Initial NavBar commit

---
+++ src/NavBar.js
@@ -0,0 +1,51 @@
+// Navbar.js
+import './style.css'; // Import the CSS file
+import React, { useState } from 'react';
+
+const Navbar = () => {
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+    const toggleSearchVisibility = () => {
+        setIsSearchVisible(!isSearchVisible);
+    };
+
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
+            <div className="container-fluid d-flex justify-content-center align-items-center">
+                <a className="navbar-brand" href="#">FooBar</a>
+
+                <form className="d-flex">
+                    {/* Search input for larger screens */}
+                    <input
+                        className="form-control me-2 d-none d-lg-block"
+                        type="search"
+                        placeholder="Search in FooBar"
+                        aria-label="Search" />
+
+                    {/* Search button for smaller screens */}
+                    <button
+                        className="btn btn-outline-primary d-lg-none"
+                        onClick={toggleSearchVisibility}
+                        type="button"
+                    >
+                        <i class="bi bi-search"></i>
+                    </button>
+
+
+                    {/* Search input for smaller screens (conditionally rendered based on state) */}
+                    {isSearchVisible && (
+                        <input
+                            className="form-control me-2 d-block d-lg-none"
+                            type="search"
+                            placeholder="Search in FooBar"
+                            aria-label="Search"
+                        />
+                    )}
+                </form>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
